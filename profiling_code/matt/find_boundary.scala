01e355b27e97f9cf2447bff1c28ac44594582988
Matthew <76405126+23matthewc@users.noreply.github.com>
2026-04-21 14:32:25 -0400

    Create find_boundary.scala

---
+++ profiling_code/matt/find_boundary.scala
@@ -0,0 +1,14 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+val spark = SparkSession.builder().appName("CoordinateProfiling").getOrCreate()
+val df = spark.read.option("header", "true").option("inferSchema", "true").csv("hdfs:///user/mc9967_nyu_edu/final_project/*.csv")
+
+val coordStats = df.select(
+    min("start_latitude").alias("min_lat"),
+    max("start_latitude").alias("max_lat"),
+    min("start_longitude").alias("min_lon"),
+    max("start_longitude").alias("max_lon")
+)
+
+coordStats.show()
